0693e77ba1055ae3bc0b863c1bb87f9549509e29
kunalmodak123 <122813412+kunalmodak123@users.noreply.github.com>
2024-10-27 16:19:11 +0530

    update

--- src/test/java/logger4jDemo/CrossBrowserwithLog4j.java
+++ src/test/java/logger4jDemo/CrossBrowserwithLog4j.java
@@ -27,20 +27,18 @@ public class CrossBrowserwithLog4j {
 		@Parameters("browser") // it is nothing but an annotation which pass value from testng.xml parameter value to an method also it help to identify where to pass that parameter xml value.
 		@BeforeMethod
 		public void OpenBrowser(String browser) {
-			if (browser.equals("chrome")) 
-			{
+			
+			switch (browser) {
+			case "chrome":
 				w = new ChromeDriver();	
 				log.info("Chrome browser has been launch");
-			} else if (browser.equals("firefox")) 
-			{
-				w = new FirefoxDriver();
-				log.info("Forefox browser has been launch");
+				break;
+				
+			case "firefox":
+				w = new FirefoxDriver();	
+				log.info("Firefox browser has been launch");
+				break;
 			}
-				else if (browser.equals("edge")) 
-//			{
-//				w = new EdgeDriver();
-//				log.info("Edge browser has been launch");
-//			}
 			w.get("https://demo.frontaccounting.eu/index.php");
 			log.info("Browser has been redirected to Front Accounting site");
 		}
